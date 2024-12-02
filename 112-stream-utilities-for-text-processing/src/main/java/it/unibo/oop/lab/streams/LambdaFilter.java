f6f399ba7bdecda540d023db7fc584e6519db067
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-12-02 18:21:54 +0100

    112: LambdaFilter: Implemented command WORD_COUNT

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -44,15 +44,22 @@ public final class LambdaFilter extends JFrame {
          * Commands.
          */
         IDENTITY("No modifications", Function.identity()),
-        TO_LOWERCASE("Convert to lowercase", String::toLowerCase),
-        CHARS_COUNT("Count the number of chars", cnt -> Long.toString(cnt.chars().filter(eol -> eol == '\n' ).count() + 1)),
-        ALPHAB_ORDER("List all the words in alphabetical order", words ->
+        TO_LOWERCASE("To lowercase", String::toLowerCase),
+        CHARS_COUNT("Chars counter", cnt -> Long.toString(cnt.chars().filter(eol -> eol == '\n' ).count() + 1)),
+        ALPHAB_ORDER("Alphabetical order", words ->
             Arrays.stream(words.split(PATTERN.toString()))
                 .sorted()
                 .collect(Collectors.joining("\n"))
+        ),
+        WORD_COUNT("Word with count", words ->
+            Arrays.stream(words.split(PATTERN.toString()))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(it -> it.getKey() + " -> " + it.getValue())
+                .collect(Collectors.joining("\n"))
         );
-
-
+        
         private final String commandName;
         private final Function<String, String> fun;
 
